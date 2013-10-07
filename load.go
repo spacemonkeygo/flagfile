17dc3442c15d143bcdc2df99828e7a6b4457a465
JT Olds <jt@spacemonkey.com>
2013-10-07 16:22:54 -0600

    space monkey internal commit export
    [katamari commit: ba258db12d7e508afe066306d696a0cb6913345a]

--- load.go
+++ load.go
@@ -12,47 +12,53 @@ import (
 	"strings"
 )
 
-var flagfile = flag.String("flagfile", "", "a file from which to load flags")
+var flagfile = flag.String("flagfile", "", "a file (or multiple files, "+
+	"comma-separated) from which to load flags")
 
 func Load() {
 	flag.Parse()
-	if len(*flagfile) == 0 {
-		return
-	}
 
 	set_flags := map[string]bool{}
 	flag.Visit(func(f *flag.Flag) {
 		set_flags[f.Name] = true
 	})
 
-	fh, err := os.Open(*flagfile)
-	if err != nil {
-		panic(fmt.Sprintf("unable to open flagfile '%s': %s", *flagfile, err))
-	}
-	defer fh.Close()
+	files := strings.Split(*flagfile, ",")
+	for _, file := range files {
+		if len(file) == 0 {
+			continue
+		}
+		func() {
+			fh, err := os.Open(file)
+			if err != nil {
+				panic(fmt.Sprintf("unable to open flagfile '%s': %s", file, err))
+			}
+			defer fh.Close()
 
-	scanner := bufio.NewScanner(fh)
-	for scanner.Scan() {
-		option := strings.TrimSpace(scanner.Text())
-		if len(option) == 0 || option[0] == '#' {
-			continue
-		}
-		parts := strings.SplitN(option, "=", 2)
-		if len(parts) != 2 {
-			panic(fmt.Sprintf("Unable to parse flagfile line '%s'", option))
-		}
-		name := strings.TrimSpace(parts[0])
-		if set_flags[name] {
-			continue
-		}
-		value := strings.TrimSpace(parts[1])
-		err := flag.Set(name, value)
-		if err != nil {
-			panic(fmt.Sprintf("Unable to set flag '%s' to '%s': %s",
-				name, value, err))
-		}
-	}
-	if err = scanner.Err(); err != nil {
-		panic(fmt.Sprintf("unable to read flagfile '%s': %s", *flagfile, err))
+			scanner := bufio.NewScanner(fh)
+			for scanner.Scan() {
+				option := strings.TrimSpace(scanner.Text())
+				if len(option) == 0 || option[0] == '#' {
+					continue
+				}
+				parts := strings.SplitN(option, "=", 2)
+				if len(parts) != 2 {
+					panic(fmt.Sprintf("Unable to parse flagfile line '%s'", option))
+				}
+				name := strings.TrimSpace(parts[0])
+				if set_flags[name] {
+					continue
+				}
+				value := strings.TrimSpace(parts[1])
+				err := flag.Set(name, value)
+				if err != nil {
+					panic(fmt.Sprintf("Unable to set flag '%s' to '%s': %s",
+						name, value, err))
+				}
+			}
+			if err = scanner.Err(); err != nil {
+				panic(fmt.Sprintf("unable to read flagfile '%s': %s", file, err))
+			}
+		}()
 	}
 }
